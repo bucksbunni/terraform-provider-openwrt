40ad578b4f0d4829d168894741cb4e5c2aa47182
bucksbunni <bucksbunni@tutamail.com>
2026-03-20 17:08:03 +0100

    Add basic data source usage

---
+++ examples/basic/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    openwrt = {
+      source  = "bucksbunni/openwrt"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "openwrt" {
+  host     = "http://192.168.1.1"
+  username = "root"
+  password = "your_password"
+  insecure = true
+}
+
+data "openwrt_sys_hostname" "router" {}
+
+data "openwrt_sys_uptime" "status" {}
+
+output "hostname" {
+  description = "Router hostname"
+  value       = data.openwrt_sys_hostname.router.hostname
+}
+
+output "uptime_seconds" {
+  description = "Router uptime in seconds"
+  value       = data.openwrt_sys_uptime.status.uptime
+}
+
+output "network_interfaces" {
+  description = "Available network interfaces"
+  value       = data.openwrt_sys_net_devices.main.devices
+}
